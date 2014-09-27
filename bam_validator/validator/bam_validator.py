e6cea748666bf51c88d599b1551e745d7ff0907d
MurphyMarkW <murphy.mark.w+github@gmail.com>
2014-09-26 22:46:19 -0500

    Removed json conversions. No longer necessary.

--- bam_validator/validator/bam_validator.py
+++ bam_validator/validator/bam_validator.py
@@ -15,7 +15,7 @@ def validate():
   claim = set(state['meta']['claimed'].keys())
 
   # Get the current list of candidates.
-  candi = set(json.loads(signpost.find(settings['query'],no_data=False))['dids'])
+  candi = set(signpost.find(settings['query'])['dids'])
 
   # Find candidates not yet claimed.
   candi = claim - candi
@@ -27,14 +27,14 @@ def validate():
   state['meta']['claimed'][candi] = 'tmp_id'
 
   # Attempt to claim a candidate.
-  ret = json.loads(signpost.post(state['did'],state['rev'],json.dumps(state['meta']),meta=True))
+  ret = signpost.post(state['did'],state['rev'],json.dumps(state['meta']),meta=True)
   if 'error' in ret: return # Early return - we failed to claim a candidate.
 
   # Pull selected candidate state.
-  candi_state = json.loads(signpost.find(candi))
+  candi_state = signpost.find(candi)
 
   # Pull data from candidate data did.
-  filename = json.loads(signpost.find(candi_state['meta']['data_did'],no_data=False))['files'][0]
+  filename = signpost.find(candi_state['meta']['data_did'])['files'][0]
 
   # Validate data as bam format.
   valid = bool(subprocess.call(['picard-tools','ValidateSamFile',filename]))
@@ -46,13 +46,13 @@ def validate():
   while True:
 
     # Refresh state.
-    state = json.loads(signpost.find(settings['state']))
+    state = signpost.find(settings['state'])
 
     # Update local state with result.
     state['meta']['valid' if valid else 'invalid'].append(candi)
 
     # Attempt to push state.
-    ret = json.loads(signpost.post(state['did'],state['rev'],json.dumps(state['meta']),meta=True))
+    ret = signpost.post(state['did'],state['rev'],state['meta'],meta=True)
     if 'error' in ret: continue # Try again until success.
 
     # Successfully updated state - we're done.
