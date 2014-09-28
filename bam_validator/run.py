33a05f414fceb573ba5daaf9b259a0de2e314667
Ubuntu <ubuntu@dev-bismuth-workers.novalocal>
2014-09-28 23:19:48 +0000

    Updated for demo.

--- bam_validator/run.py
+++ bam_validator/run.py
@@ -10,4 +10,5 @@ def run():
   validator.validate()
 
 if __name__ == '__main__':
-  run()
+  #run()
+  sched.start()
--- bam_validator/validator/bam_validator.py
+++ bam_validator/validator/bam_validator.py
@@ -1,4 +1,6 @@
 import os
+import json
+import random
 import subprocess
 
 import signpostclient as signpost
@@ -18,40 +20,82 @@ def validate():
   # Find candidates not yet claimed.
   candi = candi - claim
 
+  # If non-null set, we have something to look at.
+  if not candi: return
+
   # Select a candidate.
-  candi = next(iter(candi))
+  candi = random.sample(candi,1)[0]
 
   # Update local state with a claim.
   state['meta']['claimed'][candi] = 'tmp_id'
 
   # Attempt to claim a candidate.
-  ret = signpost.post(state['did'],state['rev'],state['meta'],meta=True)
+  ret = signpost.post(state['did'],state['rev'],json.dumps(state['meta']),meta=True)
   if 'error' in ret: return # Early return - we failed to claim a candidate.
 
-  # Pull selected candidate state.
-  candi_state = signpost.find(candi)
+  print 'claimed %s' % candi
 
-  # Pull data from candidate data did.
-  filename = signpost.find(candi_state['meta']['data_did'])['files'][0]
+  # Attempt to pull down the candidate information.
+  try:
+
+    # Pull selected candidate state.
+    candi_state = signpost.find(candi)
+
+    # Pull data from candidate data did.
+    filename = signpost.find(candi_state['meta']['data_did'])['files'][0]
+
+  # Failed to pull the data, unclaim the candidate and exit gracefully.
+  except Exception as err:
+
+    print err
+    print 'unclaiming %s' % candi
+
+    # Keep trying to unclaim the candidate.
+    while True:
+
+      state = signpost.find(settings['state'])
+
+      del state['meta']['claimed'][candi]
+
+      ret = signpost.post(state['did'],state['rev'],json.dumps(state['meta']),meta=True)
+      if 'error' in ret: continue # Try again until success
+
+      print 'unclaimed %s' % candi
+
+      return
+
+  print 'pulled down %s' % filename
 
   # Validate data as bam format.
-  valid = bool(subprocess.call(['picard-tools','ValidateSamFile',filename]))
-
-  # Delete the file.
-  os.remove(filename)
+  try: valid = subprocess.check_output([
+         'picard-tools',
+         'ValidateSamFile',
+         'INPUT=%s' % filename,
+       ],stderr=subprocess.STDOUT,)
+  except Exception as err:
+    print 'file not valid bam format'
+    print err
+    return
+  else: print 'file validated as bam format'
+  finally: os.remove(filename)
 
   # Update state with results of validation.
   while True:
 
-    # Refresh state.
-    state = signpost.find(settings['state'])
+    print 'attempting to post state'
 
-    # Update local state with result.
-    state['meta']['valid' if valid else 'invalid'].append(candi)
+    output = signpost.init()
 
-    # Attempt to push state.
-    ret = signpost.post(state['did'],state['rev'],state['meta'],meta=True)
+    meta = {
+      '_type' : 'cghub_import_validation',
+      'cghub_import_did' : state['did'],
+      'picard_output' : valid,
+    }
+
+    ret = signpost.post(output['did'],output['rev'],json.dumps(meta),meta=True)
     if 'error' in ret: continue # Try again until success.
+
+    print 'posted state'
 
     # Successfully updated state - we're done.
     return
