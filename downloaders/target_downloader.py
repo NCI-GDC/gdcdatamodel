53734369acfd52be44422b96e95fdcd21f26de2a
Joshua Miller <jshuasmiller@gmail.com>
2014-12-15 13:05:51 -0600

    updates downloader class

--- downloaders/target_downloader.py
+++ downloaders/target_downloader.py
@@ -8,24 +8,29 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--download_path', required=True, type=str)
-    parser.parse_args()
     args = parser.parse_args()
 
     downloader = Downloader(
+
         # gtdownload settings
         cghub_key=os.path.expanduser('~/authorization/jmiller_cghub_key'),
+
         # filesystem settings
         download_path=args.download_path,
+
         # Signpost settings
         signpost_host='localhost',
         signpost_port='8080',
+
         # neo4j settings
         neo4j_host='localhost',
         neo4j_port='7474',
         access_group='phs0004(64|65|66|67|68|69|71)',
+
         # s3 settings
         s3_auth_path=os.path.expanduser('~/authorization/gdc.yaml'),
         s3_url='192.170.230.172',
         s3_bucket='target_cghub_protected',
+
     )
     downloader.start()
--- downloaders/tcga_downloader.py
+++ downloaders/tcga_downloader.py
@@ -8,21 +8,25 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--download_path', required=True, type=str)
-    parser.parse_args()
     args = parser.parse_args()
 
     downloader = Downloader(
+
         # gtdownload settings
         cghub_key=os.path.expanduser('~/authorization/jmiller_cghub_key'),
+
         # filesystem settings
         download_path=args.download_path,
+
         # Signpost settings
         signpost_host='localhost',
         signpost_port='8080',
+
         # neo4j settings
         neo4j_host='localhost',
         neo4j_port='7474',
         access_group='phs000178',
+
         # s3 settings
         s3_auth_path=os.path.expanduser('~/authorization/gdc.yaml'),
         s3_url='192.170.230.172',
