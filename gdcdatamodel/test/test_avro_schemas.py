7d9207bfb4957a1f57dbb8879107a8fe5330da7a
Joshua Miller <jshuasmiller@gmail.com>
2015-01-07 14:46:09 -0600

    divides test

--- gdcdatamodel/test/test_avro_schemas.py
+++ gdcdatamodel/test/test_avro_schemas.py
@@ -19,15 +19,17 @@ class TestAvroSchemaValidation(unittest.TestCase):
             'nodes',
         )
 
-    def test_example_aliquot_node(self):
+    def test_valid_aliquot_node(self):
         valid_path = os.path.join(
             self.example_node_dir, 'aliquot_valid.json')
-        invalid_path = os.path.join(
-            self.example_node_dir, 'aliquot_invalid.json')
         with open(valid_path, 'r') as f:
             valid_aliquot_json = json.loads(f.read())
+        print json.dumps(json.loads(str(node_avsc_object)), indent=2)
+        self.assertTrue(validate(node_avsc_object, valid_aliquot_json))
+
+    def test_invalid_aliquot_node(self):
+        invalid_path = os.path.join(
+            self.example_node_dir, 'aliquot_invalid.json')
         with open(invalid_path, 'r') as f:
             invalid_aliquot_json = json.loads(f.read())
-
-        self.assertTrue(validate(node_avsc_object, valid_aliquot_json))
         self.assertFalse(validate(node_avsc_object, invalid_aliquot_json))
