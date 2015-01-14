6e6eab9595132ff43026d0575386f8992d7fee49
James Porter <porterjamesj@gmail.com>
2015-01-14 17:21:08 -0600

    fix some logging

--- zug/datamodel/tcga_dcc_sync.py
+++ zug/datamodel/tcga_dcc_sync.py
@@ -199,7 +199,7 @@ def classify(archive, filename):
                         else:
                             result[field] = match.groups()[i]
             return result
-    raise RuntimeError("file {}/{}failed to classify".format(archive["archive_name"], filename))
+    raise RuntimeError("file {}/{} failed to classify".format(archive["archive_name"], filename))
 
 
 class TCGADCCArchiveSyncer(object):
@@ -303,7 +303,7 @@ class TCGADCCArchiveSyncer(object):
                                                        property_matches={"name": val},
                                                        session=session)
             if not attr_node:
-                self.log.error("attr_node with label %s and name %s not found (trying to tie for file) ", attr, val, file_node["file_name"])
+                self.log.error("attr_node with label %s and name %s not found (trying to tie for file %s) ", attr, val, file_node["file_name"])
             edge_to_attr_node = PsqlEdge(label=LABEL_MAP[attr],
                                          src_id=file_node.node_id,
                                          dst_id=attr_node.node_id)
