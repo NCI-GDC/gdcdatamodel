d577f2a9945fb15fc3d6b0a33de1cba272f0b0bc
Joshua Miller <jshuasmiller@gmail.com>
2015-01-27 17:30:32 -0600

    updates setup and adds null option to gender in clinical schema

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     packages=["gdcdatamodel"],
     install_requires=[
         'avro==1.7.7',
-        'graphviz',
+        'graphviz==0.4.2',
+        'addict==0.2.7'
     ],
     package_data={
         "gdcdatamodel": [
