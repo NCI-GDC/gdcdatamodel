19f2c7143dd9fbdb50333d1fab7012826e1e8f33
Joshua Miller <jshuasmiller@gmail.com>
2015-01-27 18:24:50 -0600

    initial patricipant json generation wip

---
+++ bin/generate_es_json.py
@@ -0,0 +1,57 @@
+import logging
+import argparse
+import re
+from pprint import pprint
+from multiprocessing import Pool
+from zug.datamodel import Psqlgraph2JSON
+from cdisutils.log import get_logger
+
+log = get_logger("dcc_bio_importer")
+logging.root.setLevel(level=logging.ERROR)
+
+re_biospecimen = re.compile(".*(biospecimen).*\\.xml")
+re_clinical = re.compile(".*(clinical).*\\.xml")
+all_reg = ".*(bio).*(Level_1).*\\.xml"
+args = None
+
+# These will be set when the process each initialized
+converter = None
+
+
+def get_converter():
+    return Psqlgraph2JSON.psqlgraph2json(
+        host=args.host,
+        user=args.user,
+        password=args.password,
+        database=args.database,
+    )
+
+
+def initializer(*args):
+    global converter
+    converter = get_converter()
+
+
+def process(archive):
+    pass
+
+
+def start_conversion():
+    converter = get_converter()
+    converter.walk_participants()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--database', default='gdc_datamodel', type=str,
+                        help='the database to import to')
+    parser.add_argument('-u', '--user', default='test', type=str,
+                        help='the user to import as')
+    parser.add_argument('-p', '--password', default='test', type=str,
+                        help='the password for import user')
+    parser.add_argument('-i', '--host', default='localhost', type=str,
+                        help='the postgres server host')
+    parser.add_argument('-n', '--nproc', default=8, type=int,
+                        help='the number of processes')
+    args = parser.parse_args()
+    start_conversion()
---
+++ zug/datamodel/psqlgraph2json.py
@@ -0,0 +1,47 @@
+import psqlgraph
+import itertools
+from gdcdatamodel.mappings import get_file_es_mapping, \
+    get_participant_es_mapping
+
+
+class Psqlgraph2JSON(object):
+
+    """
+    """
+
+    def __init__(self, host, user, password, database,
+                 node_validator=None):
+        """Walks the graph to produce elasticsearch json documents.
+        Assumptions include:
+
+        """
+        self.graph = psqlgraph.PsqlGraphDriver(
+            host=host, user=user, password=password, database=database)
+        self.files, self.participants = [], []
+        self.batch_size = 10
+        self.leaf_nodes = ['center']
+        self.file_mapping = get_file_es_mapping()
+        self.participant_mapping = get_participant_es_mapping()
+
+    def walk_participant_node(self, node, mapping, path=[], level=0):
+        for neighbor, label in itertools.chain(
+                [(a.src, a.label) for a in node.edges_in],
+                [(b.dst, b.label) for b in node.edges_out]):
+            print '|    '*level+'+', '{} - {} - {}'.format(
+                node.label, label, neighbor.label)
+            if neighbor.label not in mapping.keys()\
+               or node.node_id in path or neighbor.label in self.leaf_nodes:
+                continue
+            self.walk_participant_node(
+                neighbor, mapping[neighbor.label],
+                path+[node.node_id], level+1)
+
+    def get_participants(self):
+        self.participants = self.graph.node_lookup(label='participant')\
+                                      .yield_per(self.batch_size)\
+                                      .limit(1)
+
+    def walk_participants(self):
+        self.get_participants()
+        for f in self.participants:
+            self.walk_participant_node(f, self.participant_mapping)
