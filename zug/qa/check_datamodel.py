451d4c7e75a94c71f87a9cf09f357ebfc5a11c11
Joshua Miller <jshuasmiller@gmail.com>
2015-01-30 10:13:02 -0600

    adds module for qa

---
+++ zug/qa/check_datamodel.py
@@ -0,0 +1,19 @@
+import psqlgraph
+
+
+class CheckDatamodel(object):
+
+    """
+    """
+
+    def __init__(self, host, user, password, database,
+                 node_validator=None):
+        """Walks the graph to produce elasticsearch json documents.
+        Assumptions include:
+
+        """
+        self.graph = psqlgraph.PsqlGraphDriver(
+            host=host, user=user, password=password, database=database)
+
+    def get_participant_count(self):
+        return self.graph.node_lookup(label='participant').count()
