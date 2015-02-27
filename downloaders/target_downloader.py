e1144828f55c66d8990f0083a632fbcee5adfa94
James J Porter <porterjamesj@gmail.com>
2015-02-27 12:12:01 -0600

    back to cleversafe

--- downloaders/target_downloader.py
+++ downloaders/target_downloader.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         resume=args.resume,
 
         # s3 settings
-        s3_auth_path=os.path.expanduser('~/authorization/ceph.yaml'),
+        s3_auth_path=os.path.expanduser('~/authorization/cleversafe.yaml'),
         s3_url=args.gateway,
         s3_bucket='target_cghub_protected',
         s3_port=args.port,
