96fbfe002232278c5ae2df73549a1308fba99ade
Joshua Miller <jshuasmiller@gmail.com>
2015-03-02 13:15:47 -0600

    update gdcdatamodel hash

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@0caa0a8ea98e8dee88fe6c7a8a6e15c2d9da999e#egg=psqlgraph',
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@6c3138bb946da6b68f860ed495f2889517a3b565#egg=cdisutils',
-        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@9af3a6dfae27376130b2c56268f0605230de5be8#egg=gdcdatamodel',
+        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@3b68b4ab3dfe63ab55b3d45e272bfeb140428f1f#egg=gdcdatamodel',
         'git+ssh://git@github.com/NCI-GDC/python-signpostclient.git@381e41d09dd7a0f9cd5f1ea5abea5bb1f34e9e70#egg=signpostclient',
     ]
 )
