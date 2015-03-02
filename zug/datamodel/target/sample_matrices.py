ab3fc8c856747f9bc74c22d88db784a8a3dedbc6
James Porter <porterjamesj@gmail.com>
2015-03-02 17:49:25 -0600

    wip sample matrix import

--- zug/datamodel/target/sample_matrices.py
+++ zug/datamodel/target/sample_matrices.py
@@ -8,8 +8,12 @@ import pandas as pd
 from lxml import html
 from collections import defaultdict
 from cdisutils.log import get_logger
+from uuid import UUID, uuid5
 
-PROJECTS = ["ALL-P1", "ALL-P2", "AML", "CCSK", "NBL", "OS", "RT", "WT"]
+PROJECTS = ["ALL-P1", "ALL-P2", "AML", "AML-IF", "CCSK", "NBL", "OS", "RT", "WT"]
+
+NAMESPACE_PARTICIPANTS = UUID('6e201b2f-d528-411c-bc21-d5ffb6aa8edb'),
+NAMESPACE_SAMPLES = UUID('90383d9f-5124-4087-8d13-5548da118d68')
 
 TUMOR_CODE_TO_DESCRIPTION = {
     "00": "Non cancerous tissue",
@@ -41,28 +45,36 @@ TUMOR_CODE_TO_DESCRIPTION = {
 SAMPLE_TYPE_TO_DESCRIPTION = {
     "01": "Primary Tumor",
     "02": "Recurrent Tumor",
-    "03": "Primary Blood Cancer",
-    "04": "Recurrent Blood Cancer",
+    "03": "Primary Blood Derived Cancer - Peripheral Blood",
+    "04": "Recurrent Blood Derived Cancer - Peripheral Blood",
     "05": "Additional - New Primary",
     "06": "Metastatic",
     "07": "Additional Metastatic",
     "08": "Post neo-adjuvant therapy",
-    "09": "Primary Blood Cancer BM",
+    "09": "Primary Blood Derived Cancer - Bone Marrow",
     "10": "Blood Derived Normal",
     "11": "Solid Tissue Normal",
     "12": "Buccal Cell Normal",
-    "13": "EBV Normal",
-    "14": "BM Normal",
-    "15": "Fibroblast Normal",
-    "20": "Cell Line Control",
-    "40": "Recurrent Blood Cancer",
-    "41": "Post treatment Blood Cancer Bone Marrow ",
-    "42": "Post treatment Blood Cancer Blood",
-    "50": "Cancer cell line",
-    "60": "Xenograft, primary",
-    "61": "Xenograft, cell-line derived",
+    "13": "EBV Immortalized Normal",
+    "14": "Bone Marrow Normal",
+    "15": "Fibroblasts from Bone Marrow Normal",
+    "20": "Control Analyte",
+    "40": "Recurrent Blood Derived Cancer - Peripheral Blood",
+    "41": "Blood Derived Cancer - Bone Marrow, Post-treatment",
+    "42": "Blood Derived Cancer - Peripheral Blood, Post-treatment",
+    "50": "Cell Lines",
+    "60": "Primary Xenograft Tissue",
+    "61": "Cell Line Derived Xenograft Tissue",
     "99": "Granulocytes",
 }
+
+
+def split_seq(iterable, size):
+    it = iter(iterable)
+    item = list(itertools.islice(it, size))
+    while item:
+        yield item
+        item = list(itertools.islice(it, size))
 
 
 def sample_for(aliquot):
@@ -78,6 +90,15 @@ def parse_metadata(sample):
     return tumor_code, tissue_code
 
 
+def filter_pending(aliquots):
+    """Some aliquots are of the form TARGET-52-NAAELV-50A-01R [P-SM],
+    which apparently means that the target folks are still waiting on
+    data about this aliquot, so we should skip these for now.
+    """
+    return [aliquot for aliquot in aliquots
+            if not re.search("\[P-[A-Z]{2}\]", aliquot)]
+
+
 class TARGETSampleMatrixImporter(object):
 
     def __init__(self, project, graph=None, dcc_auth=None):
@@ -86,17 +107,14 @@ class TARGETSampleMatrixImporter(object):
         self.dcc_auth = dcc_auth
         self.log = get_logger("target_sample_matrix_import_{}_{}".format(os.getpid(), self.project))
 
-    def locate_sample_matrix_for_project(self):
+    def locate_sample_matrix(self):
         """Given a project, return the url to it's latest sample matrix."""
         if self.project == "ALL-P1":
             search_url = "https://target-data.nci.nih.gov/ALL/Phase_I/Discovery/SAMPLE_MATRIX/"
-            template = "TARGET_ALLP1_SampleMatrix_([0-9]*)\.xlsx"
         elif self.project == "ALL-P2":
             search_url = "https://target-data.nci.nih.gov/ALL/Phase_II/Discovery/SAMPLE_MATRIX/"
-            template = "TARGET_ALLP2_SampleMatrix_([0-9]*)\.xlsx"
-        elif self.project in ["AML", "CCSK", "NBL", "OS", "RT", "WT"]:
+        elif self.project in PROJECTS:
             search_url = "https://target-data.nci.nih.gov/{}/Discovery/SAMPLE_MATRIX/".format(self.project)
-            template = "(TARGET_)?{}_SampleMatrix_([0-9]*)\.xlsx".format(self.project)
         else:
             raise RuntimeError("project {} is not known".format(self.project))
         resp = requests.get(search_url, auth=self.dcc_auth)
@@ -104,11 +122,12 @@ class TARGETSampleMatrixImporter(object):
         search_html = html.fromstring(resp.content)
         links = search_html.cssselect('a')
         for link in links:
-            maybe_match = re.match(template, link.attrib["href"])
+            maybe_match = re.search("SampleMatrix_([0-9]{8})", link.attrib["href"])
             if maybe_match:
                 self.url = urljoin(search_url, link.attrib["href"])
-                self.version = maybe_match.groups()[-1]
+                self.version = int(maybe_match.group(1))
                 return
+        raise RuntimeError("Could not find sample matrix at url {}".format(search_url))
 
     def load_sample_matrix(self, url):
         """Given a url, load a sample matrix from it into a pandas DataFrame"""
@@ -151,6 +170,7 @@ class TARGETSampleMatrixImporter(object):
         # this amounts to flattening the nested list
         aliquots = list(itertools.chain(*aliquot_lists))
         aliquots = [aliquot.strip() for aliquot in aliquots]  # some of them have suprious whitespace on the front
+        aliquots = filter_pending(aliquots)
         for aliquot in aliquots:
             assert aliquot.startswith(participant_id)
             assert len(aliquot.split("-")) == 5
@@ -166,10 +186,15 @@ class TARGETSampleMatrixImporter(object):
         mapping = {}
         for _, row in df.iterrows():
             participant_id = row[CASE_USI]
+            # TODO filter the row if the "TARGET Case" column is "N".
+            # we're not sure this is actually the right column, which
+            # is why I haven't done it yet
             if participant_id:
                 participant_id = participant_id.strip()
+                if not participant_id.startswith("TARGET"):
+                    self.log.info("Skipping Case USI: %s because it doesn't start with 'TARGET-'", participant_id)
+                    continue
                 assert len(participant_id.split("-")) == 3
-                assert participant_id.startswith("TARGET")
             else:
                 continue
             if mapping.get(participant_id):
@@ -181,7 +206,119 @@ class TARGETSampleMatrixImporter(object):
                     self.log.warning("mapping for participant %s is empty", participant_id)
         return mapping
 
+    def sanity_check(self, mapping):
+        """Sanity check the data: all samples derived from a participant should
+        start with that participant's barcode, all aliquots derived
+        from a sample should start with that sample's barcode."""
+        for participant, samples in mapping.iteritems():
+            for sample, contents in samples.iteritems():
+                assert sample.startswith(participant)
+                for aliquot in contents["aliquots"]:
+                    assert aliquot.startswith(sample)
+
     def compute_mapping(self):
-        self.locate_sample_matrix_for_project()
+        self.locate_sample_matrix()
         df = self.load_sample_matrix(self.url)
-        return self.compute_mapping_from_df(df)
+        mapping = self.compute_mapping_from_df(df)
+        self.sanity_check(mapping)
+        return mapping
+
+    def get_aliquots_from_cghub(self, mapping):
+        aliquots = []
+        for participant, samples in mapping.iteritems():
+            for sample, contents in samples.iteritems():
+                for aliquot in contents["aliquots"]:
+                    aliquots.append(aliquot)
+        results = {}
+        for group in split_seq(aliquots, 200):
+            query_string = "(" + " OR ".join(group) +  ")"
+            resp = requests.get(
+                "https://cghub.ucsc.edu/cghub/metadata/analysisDetail",
+                params={"legacy_sample_id": query_string},
+                headers={"accept": "application/json"},
+            )
+            resp.raise_for_status()
+            for result in resp.json()["result_set"]["results"]:
+                if results.get(result["legacy_sample_id"]):
+                    assert results[result["legacy_sample_id"]] == result["aliquot_id"]
+                else:
+                    results[result["legacy_sample_id"]] = result["aliquot_id"]
+        for aliquot in aliquots:
+            if not results.get(aliquot):
+                results[aliquot] = None
+        return results
+
+    def create_edge(self, label, src, dst):
+        maybe_edge = self.graph.edge_lookup(
+            label=label,
+            srd_id=src.node_id,
+            dst_id=dst.node_id,
+        )
+        if not maybe_edge:
+            self.graph.edge_insert(
+                label="derived_from",
+                srd_id=src.node_id,
+                dst_id=dst.node_id,
+            )
+
+    def tie_to_project(self, part_node):
+        project_node = self.graph.nodes().props({"code": self.project}).one()
+        self.create_edge("member_of", part_node, project_node)
+
+    def put_mapping_in_pg(self, mapping):
+        aliquot_id_map = self.get_aliquots_from_cghub(mapping)
+        sysans = {
+            "source": "target_sample_matrices",
+            "group_id": self.project,
+            "version": self.version,
+        }
+        with self.graph.session_scope():
+            for participant, samples in mapping.iteritems():
+                part_node = self.graph.node_merge(
+                    node_id=str(uuid5(NAMESPACE_PARTICIPANTS, participant)),
+                    label="participant",
+                    system_annotations=sysans,
+                    properties={
+                        "submitter_id": participant,
+                        "days_to_index": None,
+                    }
+                )
+                self.tie_to_project(part_node)
+                for sample, contents in samples.iteritems():
+                    sample_node = self.graph.node_merge(
+                        node_id=str(uuid5(NAMESPACE_SAMPLES, sample)),
+                        label="sample",
+                        system_annotations=sysans,
+                        properties={
+                            "submitter_id": sample,
+                            "sample_type_id": contents["tissue_code"],
+                            "sample_type": contents["tissue_desc"],
+                            "tumor_code_id": contents["tumor_code"],
+                            "tumor_code": contents["tumor_desc"],
+                            "longest_dimension": None,
+                            "intermediate_dimension": None,
+                            "shortest_dimension": None,
+                            "initial_weight": None,
+                            "current_weight": None,
+                            "freezing_method": None,
+                            "oct_embedded": None,
+                            "time_between_clamping_and_freezing": None,
+                            "time_between_excision_and_freezing": None,
+                            "days_to_collection": None,
+                            "days_to_sample_procurement": None,
+                            "is_ffpe": None,
+                            "pathology_report_uuid": None,
+                        }
+                    )
+                    self.create_edge("derived_from", sample_node, part_node)
+                    for aliquot in contents["aliquots"]:
+                        if not aliquot_id_map.get(aliquot):
+                            self.log.info("Not inserting aliquot %s because it doesn't have an id in cghub", aliquot)
+                        else:
+                            aliquot_node = self.graph.node_merge(
+                                node_id=aliquot_id_map[aliquot],
+                                label="aliquot",
+                                system_annotations=sysans,
+                                properties={"submitter_id": aliquot}
+                            )
+                            self.create_edge("derived_from", aliquot_node, sample_node)
