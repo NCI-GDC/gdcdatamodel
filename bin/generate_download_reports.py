a33c37a48f01f277c1de22987fce79f418a0cd00
Joshua Miller <jshuasmiller@gmail.com>
2015-04-19 20:54:20 +0000

    add md5sum from list

--- bin/generate_download_reports.py
+++ bin/generate_download_reports.py
@@ -1,13 +1,80 @@
 #!/usr/bin/env python
 from psqlgraph import PsqlGraphDriver
 import argparse
+import json
+
+from zug.datamodel.latest_urls import LatestURLParser
+from zug.datamodel.tcga_magetab_sync import get_submitter_id_and_rev
 
 
-def print_source(g, source):
+def find_node(archive, nodes):
+    submitter_id, rev = get_submitter_id_and_rev(archive["archive_name"])
+    for node in nodes:
+        if node["submitter_id"] == submitter_id:
+            return node
+    return None
+
+
+def print_dcc(g):
+    results = ["\t".join(["Archive name", "Source", "Protected", "Status", "Digital ID", "Integrity Check", "md5checksum", "Metadata Status", "Comments/Notes"])]
+    archives = list(LatestURLParser())
+    nodes = g.nodes().labels("archive").all()
+    for archive in archives:
+        node = find_node(archive, nodes)
+        if not node:
+            results.append("\t".join([archive["archive_name"],
+                                     "TCGA_DCC",
+                                      str(archive["protected"]).upper(),
+                                      "NOT_STARTED",
+                                      "N/A",
+                                      "N/A", "N/A", "N/A", "NONE"]))
+        else:
+            status = "IMPORTED" if node["revision"] == archive["revision"] else "OLD_REVISION"
+            results.append("\t".join([archive["archive_name"],
+                                     "TCGA_DCC",
+                                      str(archive["protected"]).upper(),
+                                      status,
+                                      node.node_id,
+                                      "N/A", "N/A", "N/A", "NONE"]))
+        tsv_content = "\n".join(results)
+    with open('{}_report.tsv'.format('tcga_dcc'), 'w') as tsv:
+        tsv.write(tsv_content)
+
+
+def print_target_dcc(g):
+    results = ["\t".join(["File name", "Source", "Protected", "Status", "Digital ID", "Integrity Check", "md5checksum", "Metadata Status", "Comments/Notes"])]
+    nodes = g.nodes().labels("file").sysan(dict(source='target_dcc')).all()
+    for node in nodes:
+        results.append("\t".join([node["file_name"],
+                                  "TARGET_DCC",
+                                  'TRUE',
+                                  "IMPORTED",
+                                  "N/A",
+                                  "N/A", "N/A", "N/A", "NONE"]))
+    tsv_content = "\n".join(results)
+    with open('{}_report.tsv'.format('target_dcc'), 'w') as tsv:
+        tsv.write(tsv_content)
+
+
+def get_bad_files(path):
+    with open(path, 'r') as f:
+        text = f.read().strip().split('\n')
+    rows = {json.loads(':'.join(r.split(':')[1:]))['file_name'] for r in text}
+    return rows
+
+
+def is_bad_file(fname, bad_files):
+    if fname in bad_files:
+        return True
+    return False
+
+
+def print_cghub(g, source):
     """Print the totals of downloaded and total data
 
     """
 
+    bad_files = get_bad_files('actual_bad_files.log')
     files = g.nodes().sysan(dict(source=source))\
                      .props(dict(state='live')).all()
 
@@ -17,13 +84,14 @@ def print_source(g, source):
         for f in files:
             fname = '{}/{}'.format(
                 f.system_annotations['analysis_id'], f['file_name'])
+            m5 = 'NOT STARTED' if is_bad_file(fname, bad_files) else 'PASSED'
             tsv.write('\t'.join([
                 fname,
                 source.upper().replace('_', ' '),
                 'FALSE' if f.acl == ['open'] else 'TRUE',  # protected
                 'IMPORTED',                                # import state
-                # TODO: Parse for actual status!
-                'PASSED',                                  # mdsumed
+                f.node_id,
+                m5,  # md5summed
                 'N/A', 'N/A', 'N/A', 'N/A', 'NONE']) + '\n')
 
 
@@ -42,5 +110,7 @@ if __name__ == '__main__':
     g = PsqlGraphDriver(**args.__dict__)
 
     with g.session_scope() as session:
+        print_dcc(g)
+        print_target_dcc(g)
         for source in {'target_cghub', 'tcga_cghub'}:
-            print_source(g, source)
+            print_cghub(g, source)
