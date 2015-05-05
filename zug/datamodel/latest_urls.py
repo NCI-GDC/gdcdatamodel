bdd69e3145b415da1def3fd22289b32b792b8dcd
James Porter <porterjamesj@gmail.com>
2015-05-05 13:59:57 -0500

    dont reach out to latesturls for dcc sync test

--- zug/datamodel/latest_urls.py
+++ zug/datamodel/latest_urls.py
@@ -9,8 +9,8 @@ logger = get_logger(__name__)
 
 default_latest_firstline = 'ARCHIVE_NAME DATE_ADDED ARCHIVE_URL'.split()
 default_latest_url = "https://tcga-data.nci.nih.gov/datareports/resources/latestarchive"
-default_open_url = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/"
-default_protected_url = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/tcga4yeo/tumor/"
+OPEN_BASE_URL = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/"
+PROTECTED_BASE_URL = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/tcga4yeo/tumor/"
 
 
 # because python isoformat() isn't actually compliant without tz
@@ -22,19 +22,82 @@ class SimpleUTC(tzinfo):
         return timedelta(0)
 
 
+def parse_archive_name(archive):
+    archive_name = archive['archive_name']
+
+    pattern = re.compile(r"""
+    ^
+    (.+?)_   # center
+    (\w+)\.  # disease code
+    (.+?)\.  # platform
+    (.+?\.)? # data level, ABI archives do not have data level part
+    (\d+?)\. # batch
+    (\d+?)\. # revision
+    (\d+)    # series
+    $
+    """, re.X)
+
+    m = re.search(pattern, archive_name)
+    archive['center_name'] = m.group(1)
+    archive['disease_code'] = m.group(2)
+    archive['platform'] = m.group(3)
+    archive['batch'] = int(m.group(5))
+    archive['revision'] = int(m.group(6))
+
+    if m.group(4):
+        archive['data_level'] = m.group(4).replace('.', '')
+    else:
+        archive['data_level'] = None
+
+
+def parse_archive_url(archive):
+    archive_url = archive['dcc_archive_url']
+
+    if (archive_url.startswith(OPEN_BASE_URL)):
+        # open archive
+        archive['protected'] = False
+    elif (archive_url.startswith(PROTECTED_BASE_URL)):
+        # protected archive
+        archive['protected'] = True
+    else:
+        raise RuntimeError("url {} has unexpected prefix".format(archive["dcc_archive_url"]))
+
+    logger.debug('archive_url: %s' % archive_url)
+    parts = archive_url.split('/')
+
+    if (parts[8].upper() != archive['disease_code']):
+        logger.warning("Unmatched disease code between Archive URL and "
+                       "Archive Name: " + parts[8] + " vs " +
+                       archive['disease_code'])
+    if (parts[10] != archive['center_name']):
+        logger.warning("Unmatched center_name between Archive URL and "
+                       "Archive Name: " + parts[10] + " vs " +
+                       archive['center_name'])
+
+    archive['center_type'] = parts[9]
+    archive['platform_in_url'] = parts[11]
+    archive['data_type_in_url'] = parts[12]
+
+
+def parse_archive(archive_name, date_added, archive_url):
+    archive = {}
+    archive['_type'] = 'tcga_dcc_archive'
+    archive['archive_name'] = archive_name
+    archive['date_added'] = date_added
+    archive['dcc_archive_url'] = archive_url
+
+    parse_archive_name(archive)
+    parse_archive_url(archive)
+    return archive
+
+
 class LatestURLParser(object):
 
-    def __init__(self, open_url=default_open_url,
-                 latest_firstline=default_latest_firstline,
-                 latest_url=default_latest_url,
-                 protected_url=default_protected_url, constraints={},
-                 url_key=None,
-                 **kwargs):
-
-        self.open_url = open_url
+    def __init__(self, latest_firstline=default_latest_firstline,
+                 latest_url=default_latest_url, constraints={},
+                 url_key=None, **kwargs):
         self.latest_firstline = latest_firstline
         self.latest_url = latest_url
-        self.protected_url = protected_url
         self.constraints = constraints
         self.url_key = url_key
 
@@ -64,7 +127,7 @@ class LatestURLParser(object):
     def next(self):
         line = self.latest_report.pop(0)
         sline = line.strip().split('\t')
-        archive = self.parse_archive(*sline)
+        archive = parse_archive(*sline)
         for key, value in self.constraints.iteritems():
             if archive[key] != value:
                 return None
@@ -83,75 +146,6 @@ class LatestURLParser(object):
             return None
         return r.text
 
-    def parse_archive_name(self, archive):
-        archive_name = archive['archive_name']
-
-        pattern = re.compile(r"""
-        ^
-        (.+?)_   # center
-        (\w+)\.  # disease code
-        (.+?)\.  # platform
-        (.+?\.)? # data level, ABI archives do not have data level part
-        (\d+?)\. # batch
-        (\d+?)\. # revision
-        (\d+)    # series
-        $
-        """, re.X)
-
-        m = re.search(pattern, archive_name)
-        archive['center_name'] = m.group(1)
-        archive['disease_code'] = m.group(2)
-        archive['platform'] = m.group(3)
-        archive['batch'] = int(m.group(5))
-        archive['revision'] = int(m.group(6))
-
-        if m.group(4):
-            archive['data_level'] = m.group(4).replace('.', '')
-        else:
-            archive['data_level'] = None
-
-    def parse_archive_url(self, archive):
-        archive_url = archive['dcc_archive_url']
-        open_base_url = self.open_url
-        protected_base_url = self.protected_url
-
-        if (archive_url.startswith(open_base_url)):
-            # open archive
-            archive['protected'] = False
-
-        elif (archive_url.startswith(protected_base_url)):
-            # protected archive
-            archive['protected'] = True
-
-        else:
-            logger.warning('Unmatched archive URL: ' + archive_url)
-
-        logger.debug('archive_url: %s' % archive_url)
-        parts = archive_url.split('/')
-
-        if (parts[8].upper() != archive['disease_code']):
-            logger.warning("Unmatched disease code between Archive URL and "
-                           "Archive Name: " + parts[8] + " vs " +
-                           archive['disease_code'])
-        if (parts[10] != archive['center_name']):
-            logger.warning("Unmatched center_name between Archive URL and "
-                           "Archive Name: " + parts[10] + " vs " +
-                           archive['center_name'])
-
-        archive['center_type'] = parts[9]
-        archive['platform_in_url'] = parts[11]
-        archive['data_type_in_url'] = parts[12]
-
-    def parse_archive(self, archive_name, date_added, archive_url):
-        archive = {}
-        archive['_type'] = 'tcga_dcc_archive'
-        archive['archive_name'] = archive_name
-        archive['date_added'] = date_added
-        archive['dcc_archive_url'] = archive_url
-
-        self.parse_archive_name(archive)
-        self.parse_archive_url(archive)
-        return archive
 
 if __name__ == '__main__':
     lurls = LatestURLParser()
