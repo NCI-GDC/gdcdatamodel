eb5a103f67f6d964351f76013038392081b52ccf
Joshua Miller <jshuasmiller@gmail.com>
2015-05-18 18:32:54 +0000

    update psqlgraph dependency [2999], gdcdatadmodel dependency [1b74]

--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ setup(
         'filechunkio==1.6'
     ],
     dependency_links=[
-        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@cf88e61e492695768e82f564b4da87dc31c6b1c7#egg=psqlgraph',
+        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@299945c4cc24547bb9748165803b3b032e2ddf70#egg=psqlgraph',
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@6c3138bb946da6b68f860ed495f2889517a3b565#egg=cdisutils',
-        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@fc98eee69aab5a8b17685c5497c244f3844f64d1#egg=gdcdatamodel',
+        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@1b749f3c7464d5c976dd55ac1ac592a8aba6d8ff#egg=gdcdatamodel',
         'git+ssh://git@github.com/NCI-GDC/python-signpostclient.git@381e41d09dd7a0f9cd5f1ea5abea5bb1f34e9e70#egg=signpostclient',
     ]
 )
