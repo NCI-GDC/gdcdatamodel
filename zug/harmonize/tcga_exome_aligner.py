1db4aa63da6e58a6b3a07a9274e0e2d1a830321e
James Porter <porterjamesj@gmail.com>
2015-06-15 14:25:46 -0500

    wip

--- zug/harmonize/tcga_exome_aligner.py
+++ zug/harmonize/tcga_exome_aligner.py
@@ -1,6 +1,7 @@
 import os
 
 from sqlalchemy import func
+import docker
 
 from psqlgraph import PsqlGraphDriver
 from cdisutils.log import get_logger
@@ -23,16 +24,28 @@ class TCGAExomeAligner(object):
                 os.environ["PG_PASS"],
                 os.environ["PG_NAME"],
             )
-        self.workdir = os.environ["ALIGNMENT_WORKDIR"]
+        self.workdir = os.environ.get("ALIGNMENT_WORKDIR", "/mnt/alignment")
+        self.container_workdir = "/alignment"
         self.docker_image_id = os.environ["DOCKER_IMAGE_ID"]
+        # NOTE all of these path variables are relative to
+        # self.workdir. this is useful since we can describe them to
+        # the docker container relative to the path we mount the
+        # workdir into the container at
+        self.reference = os.environ.get("REFERENCE", "reference/GRCh38.d1.vd1.fa")
+        self.scratch_dir = os.environ.get("SCRATCH_DIR", "scratch")
+        self.cores = int(os.environ.get("ALIGNMENT_CORES" "8"))
+        # TODO initialize this lazily
+        self.docker = docker.Client()
         self.log = get_logger("tcga_exome_alignmer")
 
-    def find_exome_to_align(self):
+    def choose_bam_to_align(self):
         """The strategy is as follows:
 
         1) Make a subquery for all TCGA exomes
         2) Select at random a single aliquot which is the source of a file in that subquery
         3) Select the most recently updated file of the aliquot
+
+        We then set self.input_bam to that file.
         """
         tcga_exome_bam_ids = self.graph.nodes(File.node_id)\
                                        .sysan(source="tcga_cghub")\
@@ -51,8 +64,84 @@ class TCGAExomeAligner(object):
         sorted_files = sorted([f for f in aliquot.files],
                               key=lambda f: f.sysan["cghub_last_modified"])
         self.log.info("Aliquot has %s files", len(sorted_files))
-        file_to_work_on = sorted_files[0]
-        self.log.info("Choosing file %s to align", file_to_work_on)
-        return file_to_work_on
+        self.input_bam = sorted_files[0]
+        self.log.info("Choosing file %s to align", self.input_bam)
 
-    def align_file(file):
+    def download_file_to_tmpdir(self):
+        """TODO: need to decide how to do this. One interesting possibility
+        is to just download via the API. This is simpler in the sense
+        that it abstracts the object stores away so that this code
+        does not need access to them. However, this would configuring
+        this code with credentials to the API (and making sure they
+        didn't get rotated, etc.), and would potentially interfere
+        with the API for testing users (altough we'd prefer to
+        discover scalability issues now rather than later I
+        suppose). It would also probably be faster to hit the object
+        stores directly.
+
+        Regardless, this will need to set self.input_bam_path (which
+        should be in self.scratch_dir)
+        """
+        raise NotImplementedError()
+
+    def host_abspath(self, relative_path):
+        return os.path.join(self.workdir, relative_path)
+
+    def container_abspath(self, relative_path):
+        return os.path.join(self.container_workdir, relative_path)
+
+    def run_docker_alignment(self):
+        filtered_images = [i for i in self.docker.images()
+                           if i["Id"] == self.docker_image_id]
+        if not filtered_images:
+            raise RuntimeError("No docker image with id {} found!".format(self.docker_image_id))
+        image = filtered_images[1]
+        cmd = ("/home/ubuntu/.virtualenvs/p3/bin/python /home/ubuntu/apipe/aln.py "
+               "-r {reference_path} "
+               "-b {bam_path} "
+               "-u {file_id} "
+               "-s {scratch_dir} "
+               "-t {cores} "
+               "-l {log_dir} "
+               "-d").format(
+                   reference_path=self.container_abspath(self.reference),
+                   bam_path=self.container_abspath(self.input_bam_path),
+                   file_id=self.input_bam.node_id,
+                   scratch_dir=self.container_abspath(self.scratch_dir),
+                   cores=self.cores,
+                   log_dir=self.container_abspath(self.scratch_dir),
+               )
+        self.log.info("Creating docker container")
+        self.log.info("Docker image id: %s", image["Id"])
+        self.log.info("Docker command: %s", cmd)
+        self.log.info("Mapping host volume %s to container volume %s",
+                      self.workdir, self.container_workdir)
+        host_config = docker.utils.create_host_config(binds={
+            self.workdir: {
+                "bind": self.container_workdir,
+                "ro": False,
+            },
+        })
+        container = self.docker.create_container(
+            image=image["Id"],
+            command=cmd,
+            host_config=host_config,
+        )
+        self.log.info("Starting docker container")
+        self.docker.start(container)
+        self.log.info("Container run finished, removing")
+        self.docker.remove_container(container, v=True)
+
+    def upload_output(self):
+        """
+        1) Locate output bam
+        2) Use config to decide where to put it
+        3) Upload (this will have to use boto)
+        """
+        raise NotImplementedError()
+
+    def align(self):
+        self.download_file_to_tmpdir()
+        self.run_docker_alignment()
+        self.upload_output()
+        self.create_output_node()
