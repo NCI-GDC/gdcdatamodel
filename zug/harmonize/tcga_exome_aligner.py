f8ccdde599bd713d24744427a7f7521c65cae4e0
James Porter <porterjamesj@gmail.com>
2015-06-17 16:52:34 -0500

    upload in 1GB chunks
    IIRC ceph doesn't work if you use a chunk size smaller than this

--- zug/harmonize/tcga_exome_aligner.py
+++ zug/harmonize/tcga_exome_aligner.py
@@ -25,7 +25,7 @@ from gdcdatamodel.models import (
 )
 
 
-def read_in_chunks(file_object, chunk_size=10*1024*1024):
+def read_in_chunks(file_object, chunk_size=1024*1024*1024):
     while True:
         data = file_object.read(chunk_size)
         if not data:
