ea415b87a4d48a22db661ac17a4b1da028319589
Joe Sislow <madopal@uchicago.edu>
2015-08-10 15:46:16 +0000

    chore(setup): Match hash version for psqlgraph/gdcdatamodel
    Match the hash version with develop for the versions expected for
    gdcdatamodel and psqlgraph

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ setup(
         'pyasn1==0.1.8',
     ],
     dependency_links=[
-        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@3394989c90e1cf4bfb017c2f51d1537a03cdcc2d#egg=psqlgraph',
+        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@621d7d157fad91bd0834280b8d2c39be384dcb1a#egg=psqlgraph',
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@99c87f23c6f955fed5971f987dbe36c92c9e4596#egg=cdisutils',
-        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@aa8c797e923257ad6d77b8fadb1758e94afb113f#egg=gdcdatamodel',
+        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@264878ff7c3b591f8e272f40e489d2bda783d415#egg=gdcdatamodel',
         'git+ssh://git@github.com/NCI-GDC/python-signpostclient.git@63b3c1894299708d568506bce1ed3120fadbdf44#egg=signpostclient',
         'git+ssh://git@github.com/NCI-GDC/python-ds3-sdk.git@6a3382b3766d7e0898e9b70221d7e43f767acb8f#egg=ds3client',
     ]
