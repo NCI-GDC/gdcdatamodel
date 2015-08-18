4f4909ecf8a697c82102afcce7b24730f634e3e0
Phillis Tang <phillis.tt@gmail.com>
2015-08-18 20:48:12 +0000

    feat(validator): add tests for json validator

--- gdcdatamodel/validators/json_validators.py
+++ gdcdatamodel/validators/json_validators.py
@@ -15,12 +15,17 @@ class GDCJSONValidator(object):
                                     resolver=self.resolver)
         return validator.iter_errors(doc)
 
-    def record_errrors(self, entities):
+    def record_errors(self, entities):
         for entity in entities:
             json_doc = entity.doc
             if 'type' not in json_doc:
                 entity.record_error(
                     "'type' is a required property", key='type')
+                break
+            if json_doc['type'] not in self.schemas.schema:
+                entity.record_error(
+                    "specified type: {} is not in current data model"
+                    .format(json_doc['type']), key='type')
                 break
             for error in self.iter_errors(json_doc):
                 # the key will looks key-subkey for nested properties
--- test/test_validators.py
+++ test/test_validators.py
@@ -3,7 +3,6 @@ from gdcdatamodel.validators import GDCJSONValidator, GDCGraphValidator
 from psqlgraph import PsqlGraphDriver
 from gdcdatamodel.models import *
 from gdcapi.resources.submission.transaction import SubmissionEntity
-import logging
 
 host = 'localhost'
 user = 'test'
@@ -11,9 +10,11 @@ password = 'test'
 database = 'automated_test'
 g = PsqlGraphDriver(host, user, password, database)
 
+
 class TestValidators(unittest.TestCase):
     def setUp(self):
-        pass
+        self.graph_validator = GDCGraphValidator()
+        self.json_validator = GDCJSONValidator()
 
     def tearDown(self):
         self._clear_tables()
@@ -33,6 +34,21 @@ class TestValidators(unittest.TestCase):
 
     def test_json_validator_with_insufficient_properties(self):
         entity = SubmissionEntity()
-        entity.parse({'type':'aliquot'})
-        pass
+        entity.parse({'type': 'aliquot'})
+        self.json_validator.record_errors([entity])
+        self.assertEqual(entity.errors[0]['key'], '')
+        self.assertEqual(1, len(entity.errors))
 
+    def test_json_validator_with_wrong_node_type(self):
+        entity = SubmissionEntity()
+        entity.parse({'type': 'aliquo'})
+        self.json_validator.record_errors([entity])
+        self.assertEqual(entity.errors[0]['key'], 'type')
+        self.assertEqual(1, len(entity.errors))
+
+    def test_json_validator_with_wrong_property_type(self):
+        entity = SubmissionEntity()
+        entity.parse({'type': 'aliquot', 'alias': 1})
+        self.json_validator.record_errors([entity])
+        self.assertEqual('alias', entity.errors[0]['key'])
+        self.assertEqual(1, len(entity.errors))
