b0a7f0101df47ed66cff83dc0eedc6a096122760
Joshua Miller <jshuasmiller@gmail.com>
2015-09-02 10:03:41 -0500

    fix(ownership): update dictionary dependency

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     },
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@3394989c90e1cf4bfb017c2f51d1537a03cdcc2d#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@b6325323c0e654840de5c4192e01cc89e431d5a6#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@eca28a25f6c5c7dae678609000097df763b2bdc3#egg=gdcdictionary',
     ],
 )
