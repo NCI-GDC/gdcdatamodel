e8efff4ad77094f4ed9e7b16d81795f9175f7ad2
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-09-03 12:16:17 -0500

    feat(aligner): adds tcga_star_aligner first pass

---
+++ zug/harmonize/tcga_star_aligner.py
@@ -0,0 +1,210 @@
+import os
+import abc
+import time
+
+from gdcdatamodel.models import (
+    File,
+    FileDataFromFile
+)
+
+from zug.harmonize.abstract_harmonizer import AbstractHarmonizer
+
+
+class TCGASTARAligner(AbstractHarmonizer):
+    '''
+    STAR specialization of the AbstractHarmonizer for processing of TCGA
+    RNA-Seq data.
+    '''
+    __metaclass__ = abc.ABCMeta
+
+    def get_config(self, kwargs):
+        if os.environ.get("ALIGNMENT_SIZE_LIMIT"):
+            size_limit = int(os.environ["ALIGNMENT_SIZE_LIMIT"])
+        else:
+            size_limit = None
+        return {
+            "output_buckets": {
+                "bam": os.environ["BAM_S3_BUCKET"],
+                "bai": os.environ["BAM_S3_BUCKET"],
+                "log": os.environ["LOGS_S3_BUCKET"],
+                "db": os.environ["LOGS_S3_BUCKET"],
+                "meta": os.environ["META_S3_BUCKET"],
+            },
+            # all paths are workdir-relative. this is convenient
+            # because we can absolutize-them with respect to the
+            # container or the host.
+            # TODO FIXME update / ensure that these are still valid
+            "paths": {
+                "reference": os.environ.get("ALIGNMENT_REFERENCE",
+                                            "reference/GRCh38.d1.vd1.fa"),
+                "intervals_dir": os.environ.get("ALIGNMENT_INTERVAL_DIR",
+                                                "intervals/"),
+                "libraryname_json": os.environ.get("ALIGNMENT_LIBRARYNAME_JSON",
+                                                   "intervals/bam_libraryname_capturekey.json"),
+                "intervalname_json": os.environ.get("ALIGNMENT_INTERVALNAME_JSON",
+                                                    "intervals/bait_target_key_interval.json"),
+            },
+            "size_limit": size_limit,
+            "cores": int(os.environ.get("ALIGNMENT_CORES", "8")),
+            "force_input_id": kwargs.get("force_input_id"),
+        }
+
+    @property
+    def valid_extra_kwargs(self):
+        return ["force_input_id"]
+
+    @property
+    def input_schema(self):
+        '''
+        Mapping from names to input types.
+        '''
+        # NOTE From what james has mentioned, this is just mappings to
+        # PSQLGraph types?
+        return {
+            'fastq_tarball': File,
+        }
+
+    @property
+    def output_schema(self):
+        '''
+        Mapping from names to output types.
+        '''
+        # NOTE The reverse of the input_schema.
+        return {
+            'bam': File,
+            'bai': File,
+            'log': File,
+            # NOTE Initial pass meta will be a tarball of any excess data.
+            'meta': File,
+        }
+
+    def find_inputs(self):
+        '''
+        Identify inputs.
+        '''
+        self.log.info('Choosing FASTQ file for alignment.')
+        if self.config.get('force_input_id', False):
+            input_fastq_tarball = self.choose_fastq_by_forced_id()
+        else:
+            input_fastq_tarball = self.choose_fastq_at_random()
+        self.log.info('Selected %s for alignment.', input_fastq_tarball)
+        
+        # we expunge from this session so we can merge into another
+        # session later and load up it's classifiction nodes to
+        # classify the newly produced bam
+        self.graph.current_session().expunge(input_fastq_tarball)
+        
+        return input_fastq_tarball.node_id, {
+            'fastq_tarball': input_fastq_tarball,
+        }
+
+    def build_docker_cmd(self):
+        '''
+        Build the docker command based on configuration options.
+        '''
+        # TODO FIXME make sure that self.config and these options line up
+        return ' '.join([
+            '/usr/bin/python',
+            '/home/ubuntu/star_docker/align_star.py',
+            '--genomeDir {genome_dir}',
+            '--ref_genome {genome_ref}',
+            '--input {fastq_tarball}',
+            '--genome_annotation {genome_annotation}',
+            '--out {output_bam}',
+            '--workDir {scratch_dir}',
+            '--id {uuid}',
+            '--ref_flat {flat_ref}',
+            '--runThreadN {nthreads}',
+            # TODO FIXME same as --ref_genome in all cases?
+            '--genomeFastaFiles {genome_ref}',
+            '--rna_seq_qc_annotation {rna_seq_qc_annotation}',
+        ]).format(**self.config)
+
+    @property
+    def output_paths(self):
+        # TODO FIXME finish this
+        return {
+            "bam": self.host_abspath(
+                'output',
+                self.config['host_output_bam'],
+            ),
+            "bai": self.host_abspath(
+                'output',
+                self.config['host_output_bai'],
+            ),
+            "log": self.host_abspath(
+                self.config["scratch_dir"],
+                "aln_" + self.inputs["bam"].node_id + ".log"
+            ),
+            'meta': self.host_abspath(
+            ),
+        }
+
+    def upload_secondary_files(self):
+        """
+        Upload the log file and sqlite db to the relevant bucket
+        """
+        # TODO FIXME go over this
+        for key in ["log", "db"]:
+            path = os.path.normpath(self.host_abspath(self.output_paths[key]))
+            self.upload_file(
+                path,
+                self.config["output_buckets"][key],
+                os.path.basename(path),
+            )
+
+    def handle_output(self):
+        # TODO FIXME go over this
+        self.upload_secondary_files()
+        output_nodes = {}
+        for key in ["bam", "bai"]:
+            name = self.inputs[key].file_name.replace(".bam", "_gdc_realn.bam")
+            output_nodes[key] = self.upload_file_and_save_to_db(
+                self.host_abspath(self.output_paths[key]),
+                self.config["output_buckets"][key],
+                name,
+                self.inputs["bam"].acl
+            )
+        output_nodes["bam"].related_files = [output_nodes["bai"]]
+        docker_tag = (self.docker_image["RepoTags"][0]
+                      if self.docker_image["RepoTags"] else None)
+        edge = FileDataFromFile(
+            src=self.inputs["bam"],
+            dst=output_nodes["bam"],
+            system_annotations={
+                "alignment_started": self.start_time,
+                "alignment_finished": int(time.time()),
+                # raw_docker as opposed to whatever we might use in
+                # the future, e.g. CWL
+                "alignment_method": "raw_docker",
+                "alignment_docker_image_id": self.docker_image["Id"],
+                "alignment_docker_image_tag": docker_tag,
+                "alignment_docker_cmd": self.docker_cmd,
+                "alignment_reference_name": os.path.basename(self.config["reference"]),
+            }
+        )
+        with self.graph.session_scope() as session:
+            # merge old bam file so we can get its classification
+            session.add(self.inputs["bam"])
+            # classify new bam file, same as the old bam file
+            output_nodes["bam"].experimental_strategies = self.inputs["bam"].experimental_strategies
+            output_nodes["bam"].data_formats =  self.inputs["bam"].data_formats
+            output_nodes["bam"].data_subtypes = self.inputs["bam"].data_subtypes
+            output_nodes["bam"].platforms = self.inputs["bam"].platforms
+            # this line implicitly merges the new bam and new bai
+            session.merge(edge)
+
+    @abc.abstractmethod
+    def choose_fastq_by_forced_id(self):
+        '''
+        Return a PSQLGraph node representing the FASTQ specified by a
+        specified id.
+        '''
+        raise NotImplementedError('TODO')
+
+    @abc.abstractmethod
+    def choose_fastq_at_random(self):
+        '''
+        Return a PSQLGraph node representing a 'randomly' chosen FASTQ
+        '''
+        raise NotImplementedError('TODO')
