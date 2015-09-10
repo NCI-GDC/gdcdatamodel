c90aef01f3dd0cb2967e82bf699df2c480578b86
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-09-10 13:21:10 -0500

    feat(aligner): adds mirnaseq executable

---
+++ bin/align_tcga_mirnaseq.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from zug.binutils import zug_wrap
+from zug.harmonize.tcga_mirnaseq_aligner import TCGAMIRNASeqAligner
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--file-id", help="Force this id to be aligned")
+    args = parser.parse_args()
+    with zug_wrap():
+        aligner = TCGAMIRNASeqAligner(
+            force_input_id=args.file_id
+        )
+        aligner.go()
+
+if __name__ == "__main__":
+    main()
