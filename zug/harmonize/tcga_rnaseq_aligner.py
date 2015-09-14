b695516dbd8efe21f28ec820d03f1408679f28fa
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-09-14 15:17:48 -0500

    fix(aligner): updates fastq input query

--- zug/harmonize/tcga_rnaseq_aligner.py
+++ zug/harmonize/tcga_rnaseq_aligner.py
@@ -21,6 +21,60 @@ class TCGARNASeqAligner(TCGASTARAligner):
     def source(self):
         return "tcga_rnaseq_alignment"
 
+    @property
+    def fastq_files(self):
+        strategy = ExperimentalStrategy.name.astext == 'RNA-Seq'
+        platform = Platform.name.astext.contains('Illumina')
+        dataformat = DataFormat.name.astext.in_(['TAR', 'TARGZ'])
+        centers = Center.short_name.astext == 'UNC'
+        
+        subquery = self.graph.nodes(File.node_id)\
+            .sysan(source='tcga_cghub')\
+            .distinct(File._sysan['cghub_legacy_sample_id'].astext)\
+            .filter(File.experimental_strategies.any(strategy))\
+            .filter(File.platforms.any(platform))\
+            .filter(File.data_formats.any(dataformat))\
+            .filter(File.centers.any(centers))\
+            .order_by(
+                File._sysan['cghub_legacy_sample_id'].astext,
+                desc(File._sysan['cghub_upload_date'].cast(BigInteger)),
+            )\
+            .subquery()
+        
+        return self.graph.nodes(File).filter(File.node_id == subquery.c.node_id)
+
+    @property
+    def alignable_files(self):
+        currently_being_aligned = self.consul.list_locked_keys()
+        alignable = self.bam_files\
+            .props(state='live')\
+            .filter(~File.derived_files.any())\
+            .filter(~File.node_id.in_(currently_being_aligned))
+        
+        size_limit = self.config.get('size_limit', False)
+        if size_limit:
+            alignable = alignable.filter(
+                File.file_size.cast(BigInteger) < size_limit
+            )
+        
+        size_min = self.config.get('size_min', False)
+        if size_min:
+            alignable = alignable.filter(
+                File.file_size.cast(BigInteger) > size_min
+            )
+        
+        return alignable
+
+    def choose_fastq_at_random(self):
+        '''
+        Return a PSQLGraph node representing a 'randomly' chosen TCGA RNA-Seq
+        FASTQ File node.
+        '''
+        fastq = self.alignable_files.from_self(File).order_by(func.random()).first()
+        if not fastq:
+            raise NoMoreWorkException('We appear to have aligned all fastq files')
+        
+        return fastq
 
     def choose_fastq_by_forced_id(self):
         '''
@@ -39,46 +93,5 @@ class TCGARNASeqAligner(TCGASTARAligner):
         assert any(x.name in ['TAR', 'TARGZ'] for x in tar.data_formats)
         assert any(x.name == 'RNA-Seq' for x in tar.experimental_strategies)
         # TODO add additional constraint checks as necessary
-        
-        return tar
-
-
-    def choose_fastq_at_random(self):
-        '''
-        Return a PSQLGraph node representing a 'randomly' chosen TCGA RNA-Seq
-        FASTQ File node.
-        '''
-        # TODO verify that this filters correctly for TCGA RNA-Seq FASTQ files
-        rnaseq = ExperimentalStrategy.name.astext == "RNA-Seq"
-        illumina = Platform.name.astext.contains("Illumina")
-        tars = DataFormat.name.astext.in_(["TAR", "TARGZ"])
-        unc = Center.short_name.astext == "UNC"
-        currently_being_aligned = self.consul.list_locked_keys()
-        order = desc(File._sysan["cghub_upload_date"].cast(BigInteger)) 
-        
-        files = self.graph.nodes(File)\
-            .props(state="live")\
-            .sysan(source="tcga_cghub")\
-            .join(FileDataFromAliquot)\
-            .join(Aliquot)\
-            .distinct(Aliquot.node_id.label("aliquot_id"))\
-            .filter(File.experimental_strategies.any(rnaseq))\
-            .filter(File.platforms.any(illumina))\
-            .filter(File.data_formats.any(tars))\
-            .filter(File.centers.any(unc))\
-            .filter(~File.derived_files.any())\
-            .filter(~File.node_id.in_(currently_being_aligned))\
-            .order_by(Aliquot.node_id, order)
-        
-        size_limit = self.config.get('size_limit', False)
-        if size_limit:
-            files = files.filter(
-                File.file_size.cast(BigInteger) < size_limit
-            )
-        
-        tar = files.from_self(File).order_by(func.random()).first()
-        
-        if tar is None:
-            raise NoMoreWorkException('Could not find any unprocessed FASTQs.')
         
         return tar
