a0bee5c17f6ee86686dd503f77fcfcfed2519fa7
Joshua Miller <jshuasmiller@gmail.com>
2015-09-16 15:37:49 -0500

    feat(submission): dependency update

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     },
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@b4bb4ea9ae8631b245d61621093eb5702d33fbb7#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@339f49e5c9bc455c0871dafb9c6f15ef164818bf#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@10f8a5690a42ee659b47e56371bc4615ef845ffa#egg=gdcdictionary',
     ],
 )
