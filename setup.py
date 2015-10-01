c45b4526c0ca20ec151cea79d85e2b2d7798d8b7
Joshua Miller <jshuasmiller@gmail.com>
2015-10-01 13:35:55 -0500

    fix(clinical): update dictionary dependency

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     },
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@b4bb4ea9ae8631b245d61621093eb5702d33fbb7#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@4d2ccd79e20654e6d89d3b9cfe2f137deed5a2e5#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@4101aceb8478c22025065a151f50b9615a44f2fa#egg=gdcdictionary',
     ],
 )
