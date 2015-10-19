6523cdbb28ef8bd3aaf138d96f8002515f419af2
Joshua Miller <jshuasmiller@gmail.com>
2015-10-19 17:15:28 -0500

    fix(dependencies): bump dictionary

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@4a75cc05c7ba2174e70cca9c9ea7e93947f7a868#egg=cdisutils',
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@01fd87d4344bbdd59c494aef252764c133ad19b4#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@774441890370e265c96d7f195f412cd6a5032d76#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@a336dd7660d1d9ccb0ee084029cbdd6aba5ca756#egg=gdcdictionary',
     ],
 )
