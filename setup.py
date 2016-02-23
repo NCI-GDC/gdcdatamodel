e2b5a6c64e3a993312ce74f90d73a7bb5c53c622
Joe Sislow <madopal@uchicago.edu>
2016-02-23 11:52:56 -0600

    chore(gdcdatamodel): Version pin for fixes
    - Pin the gdcdictionary version to the hotfix

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@4a75cc05c7ba2174e70cca9c9ea7e93947f7a868#egg=cdisutils',
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@f0f198c2d7978fea311b0bc311c6db61732de261#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@ec2379c37efd5af4dfb96c643a17b0eb95e2d76a#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@adefee65384c264f21c73d9dd520b4fddfae65dd#egg=gdcdictionary',
     ],
     entry_points={
         'console_scripts': [
