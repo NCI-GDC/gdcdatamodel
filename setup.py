550b8e20d146c1cdaf2aeaded53f0f119b722796
philloooo <phillis.tt@gmail.com>
2016-03-21 11:04:14 -0500

    chore(dependency): bump datamodel, delete esbuild

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         ]
     },
     install_requires=[
+        'python-dateutil==2.4.2',
         'progressbar==2.2',
         'networkx',
         'pyyaml',
@@ -49,9 +50,9 @@ setup(
         'datadog==0.9.0',
     ],
     dependency_links=[
-        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@a00afd31a665420c5a5edd8b51fc2c7eb2889eb4#egg=psqlgraph',
+        'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@4fbeddeb058ad09741c5d2b9aacbd1c63cfc3dd1#egg=psqlgraph',
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@99c87f23c6f955fed5971f987dbe36c92c9e4596#egg=cdisutils',
-        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@8efe7cb4302a6e18cad2ae01f02f643dcf3ad6a4#egg=gdcdatamodel',
+        'git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@410db0ffcdea964f177d513b53434679a94d93d2#egg=gdcdatamodel',
         'git+ssh://git@github.com/NCI-GDC/python-signpostclient.git@ebc91a5f8343e8f4cb2caefc523b9ad3a1eb7c6c#egg=signpostclient',
         'git+ssh://git@github.com/NCI-GDC/python-ds3-sdk.git@6a3382b3766d7e0898e9b70221d7e43f767acb8f#egg=ds3client',
     ]
