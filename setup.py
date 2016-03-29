b510b15b83ba464fe7ecc14f83e284fabd25986c
philloooo <phillis.tt@gmail.com>
2016-03-29 15:47:13 -0500

    chore(dependency): bump gdcdictionary

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     dependency_links=[
         'git+ssh://git@github.com/NCI-GDC/cdisutils.git@4a75cc05c7ba2174e70cca9c9ea7e93947f7a868#egg=cdisutils',
         'git+ssh://git@github.com/NCI-GDC/psqlgraph.git@a00ec5c0d3542e6f959e82fed0e46782479c6885#egg=psqlgraph',
-        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@751fcf503a4f12367d491b1b7505d2fe100e4707#egg=gdcdictionary',
+        'git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@5450491cea2daa1197d63ef816ebc8720eb463ba#egg=gdcdictionary',
     ],
     entry_points={
         'console_scripts': [
