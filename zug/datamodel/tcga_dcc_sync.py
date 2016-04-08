a0fa32ecd4e51383d63776a8d75da5422c24bcc8
Joe Sislow <madopal@uchicago.edu>
2016-04-08 18:16:48 +0000

    fix(submitter_id): Add check for archive_node
    - Add check for archive_node

--- zug/datamodel/tcga_dcc_sync.py
+++ zug/datamodel/tcga_dcc_sync.py
@@ -780,7 +780,8 @@ class TCGADCCArchiveSyncer(object):
         if not self.get_archive():
             # if this returns None, it means we're all done
             return
-        self.project_id = self.archive_node.project_id
+        if self.archive_node:
+            self.project_id = self.archive_node.project_id
         self.log.info("syncing archive %s", self.name)
         self.archive["non_tar_url"] = self.archive["dcc_archive_url"].replace(".tar.gz", "")
         self.acl = ["phs000178"] if self.archive["protected"] else ["open"]
