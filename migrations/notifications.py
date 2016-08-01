c049979608270f2b45d0c076363f84ecc2fde42f
Joshua Miller <jsmiller@uchicago.edu>
2016-08-01 16:09:37 -0500

    feat(notificaitons): add model and migration

---
+++ migrations/notifications.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+migrations.notifications
+----------------------------------
+
+Create `notifications` table.
+"""
+
+from gdcdatamodel.models.notifications import Base, Notification
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def up(connection):
+    logger.info('Migrating notifications: up')
+
+    Base.metadata.create_all(connection)
+
+
+def down(connection):
+    logger.info('Migrating notifications: down')
+
+    sql_cmd = "DROP TABLE {}".format(Notification.__tablename__)
+    connection.execute(sql_cmd)
