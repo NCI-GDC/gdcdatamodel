7a07ee08efec21459ef0d44d8b12b717916779d5
Joe Sislow <madopal@uchicago.edu>
2017-07-19 16:34:40 +0000

    feat(node_shrouder): Add methods argument
    - Add methods argument to handle different methods of shrouding
    a node

--- migrations/node_shrouder.py
+++ migrations/node_shrouder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """gdcdatamodel.migrations.node_shrouder
-----------------------------------
+----------------------------------------
 
 Given requests to make certain nodes not visible, this code will take
 a UUID or file with a list of UUIDs and make sure they're not visible
@@ -23,8 +23,6 @@ import logging
 from sqlalchemy import not_, or_, and_
 from psqlgraph import Node, PsqlGraphDriver
 from gdcdatamodel import models as md
-from multiprocessing import Process, cpu_count, Queue
-from collections import namedtuple
 from argparse import ArgumentParser
 import datetime
 
@@ -68,10 +66,32 @@ def load_data_file(file_name):
 
     return key_data
 
+def guess_field(field_names):
+    guessed_fields = []
+    guessed_field = None
+    for key in file_data[0].keys():
+        if 'uuid' in key.lower():
+            guessed_fields.append(key)
+        elif 'id' in key.lower():
+            guessed_fields.append(key)
+    if len(guessed_fields) == 1:
+        guessed_field = guessed_fields[0]
+    elif len(guessed_fields) > 1:
+        logger.error('Unable to guess from these:')
+        logger.error(guessed_fields)
+        logger.error('Please give a field with --uuid_file_field')
+        logger.error(file_data[0].keys())
+    else:
+        logger.error('Unable to guess a field in the data, please give one of these as --uuid_file_field')
+        logger.error(file_data[0].keys())
+
+    return guessed_field
+
 def shroud_nodes(db_kwargs=None,
                  uuid_list=None,
                  count=None,
-                 dry_run=False):
+                 dry_run=False,
+                 method=None):
     graph = PsqlGraphDriver(**db_kwargs)
     total_count = 0
     shrouded_count = 0
@@ -80,9 +100,12 @@ def shroud_nodes(db_kwargs=None,
         for entry in uuid_list:
             node = graph.nodes().get(entry)
             if node:
-                logger.info('Shrouding {}'.format(entry))
-                # blank the acl so it's not visible
-                node.acl = []
+                logger.info('Shrouding {} with {}'.format(node, method))
+                if method == 'acl':
+                    # blank the acl so it's not visible
+                    node.acl = []
+                if method == 'state':
+                    node.state = 'uploaded'
 
                 # update the sysan
                 node.sysan['shrouded'] = shrouded_time
@@ -102,12 +125,18 @@ def shroud_nodes(db_kwargs=None,
 if __name__ == '__main__':
     parser = ArgumentParser()
 
-    parser.add_argument("--uuids",
+    parser.add_argument('--uuids',
         help='list of UUIDs to make invisible',
         nargs='+')
-    parser.add_argument("--uuid_file",
+    parser.add_argument('--method',
+        help='method to use to shroud nodes',
+        choices=['acl','state'],
+        default='acl')
+    parser.add_argument('--uuid_file',
         help='file with list of UUIDs to make invisible')
-    parser.add_argument("--dry_run",
+    parser.add_argument('--uuid_file_field',
+        help='field name to read UUID from in file')
+    parser.add_argument('--dry_run',
         help='do not commit results',
         action='store_true')
     parser.add_argument('--count',
@@ -129,14 +158,25 @@ if __name__ == '__main__':
         uuids = []
         if args.uuid_file:
             file_data = load_data_file(args.uuid_file)
-            uuids = [line['file_id'] for line in file_data]
+            if args.uuid_file_field:
+                if args.uuid_file_field in file_data[0].keys():
+                    uuids = [line[args.uuid_file_field] for line in file_data]
+                else:
+                    logger.error('Unable to find {} in fields, is it one of these?'.format(args.uuid_file_field))
+                    logger.error(file_data[0].keys())
+            else:
+                logger.warn('No field provided for UUIDs, doing the best I can')
+                guessed_field = guess_field(file_data[0].keys())
+                if guessed_field:
+                    uuids = [line[guessed_field] for line in file_data]
         else:
             uuids = args.uuids
 
         shroud_nodes(db_kwargs=db_kwargs,
                      uuid_list=uuids,
                      dry_run=args.dry_run,
-                     count=args.count
+                     count=args.count,
+                     method=args.method
         )
         
 
