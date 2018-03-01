1feaf65c2bbf1f97926bb1f91e2761efef150ed2
philloooo <phillis.tt@gmail.com>
2018-03-01 16:25:47 -0600

    chore(dictionary): remove gdcdictionary req

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
         'graphviz==0.4.2',
         'jsonschema==2.5.1',
         'psqlgraph',
-        'gdcdictionary',
         'dictionaryutils>=1.2.0,<2.0.0',
         'cdisutils',
         'python-dateutil==2.4.2',
@@ -21,7 +20,6 @@ setup(
     dependency_links=[
         'git+https://github.com/NCI-GDC/cdisutils.git@4a75cc05c7ba2174e70cca9c9ea7e93947f7a868#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@7b5de7d56aa3159a9526940eb273579ddbf084ca#egg=psqlgraph',
-        'git+https://github.com/NCI-GDC/gdcdictionary.git@86a4e6dd7b78d50ec17dc4bcdd1a3d25c658b88b#egg=gdcdictionary',
     ],
     entry_points={
         'console_scripts': [
