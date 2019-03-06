d1a73814803c5d7fe9eb3b4b98821238a721cf7a
Rowland Ogwara <rogwara@uchicago.edu>
2019-03-06 17:19:39 -0600

    dictionary utils update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'python-dateutil==2.4.2',
         'psqlgraph',
         'gdcdictionary',
-        'dictionaryutils>=2.0.4,<3.0.0',
+        'dictionaryutils',
         'cdisutils',
     ],
     package_data={
@@ -19,6 +19,7 @@ setup(
         ]
     },
     dependency_links=[
+        'git+ssh://git@github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils',
         'git+https://github.com/NCI-GDC/cdisutils.git@863ce13772116b51bcf5ce7e556f5df3cb9e6f63#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@1.2.0#egg=psqlgraph',
         'git+https://github.com/NCI-GDC/gdcdictionary.git@1.15.0#egg=gdcdictionary',
