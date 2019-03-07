1b7051cab333e1d89fa7b8088d81fab72fdfa95f
Rowland Ogwara <rogwara@uchicago.edu>
2019-03-07 11:06:16 -0600

    deps to https

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'graphviz==0.4.2',
         'jsonschema==2.5.1',
         'python-dateutil==2.4.2',
-        'dictionaryutils==2.0.4',
+        'dictionaryutils',
         'gdcdictionary',
         'psqlgraph',
         'cdisutils',
@@ -19,7 +19,7 @@ setup(
         ]
     },
     dependency_links=[
-        'git+ssh://git@github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils',
+        'git+https://github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils',
         'git+https://github.com/NCI-GDC/cdisutils.git@863ce13772116b51bcf5ce7e556f5df3cb9e6f63#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@1.2.0#egg=psqlgraph',
         'git+https://github.com/NCI-GDC/gdcdictionary.git@1.15.0#egg=gdcdictionary',
