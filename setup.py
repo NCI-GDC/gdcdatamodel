9f52c08e8fa9937813eb964d00b7c8bdac94e72f
Rowland Ogwara <rogwara@uchicago.edu>
2019-03-14 10:14:00 -0500

    removed diagnosis.vital_status added value mappings

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'git+https://github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils',
         'git+https://github.com/NCI-GDC/cdisutils.git@863ce13772116b51bcf5ce7e556f5df3cb9e6f63#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@1.2.0#egg=psqlgraph',
-        'git+https://github.com/NCI-GDC/gdcdictionary.git@31512006338a4e365b1bdd023814dde0b6e55938#egg=gdcdictionary',
+        'git+https://github.com/NCI-GDC/gdcdictionary.git@ab9c1ed756c626f363b1d41fa40402c2e210d2a2#egg=gdcdictionary',
     ],
     entry_points={
         'console_scripts': [
