ba772f8f2d7e624c1ae000b0d1b1951d13e43ca3
Patrick Huarng <huarngpa@gmail.com>
2019-06-26 23:04:07 -0500

    Feat/tt 1058 separate non graph models from gdcdatamodel (#280)
    * Separates non graph models to a separate repository

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
         'graphviz==0.4.2',
         'jsonschema==2.5.1',
         'python-dateutil==2.4.2',
+        'pyasn1==0.4.2',
         'dictionaryutils',
         'gdcdictionary',
         'psqlgraph',
         'cdisutils',
+        'gdc_ng_models',
     ],
     package_data={
         "gdcdatamodel": [
@@ -23,6 +25,7 @@ setup(
         'git+https://github.com/NCI-GDC/cdisutils.git@863ce13772116b51bcf5ce7e556f5df3cb9e6f63#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@1.2.0#egg=psqlgraph',
         'git+https://github.com/NCI-GDC/gdcdictionary.git@1.17.0#egg=gdcdictionary',
+        'git+https://github.com/NCI-GDC/gdc-ng-models.git@1.0.0#egg=gdc_ng_models',
     ],
     entry_points={
         'console_scripts': [
