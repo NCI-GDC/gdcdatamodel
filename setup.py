c0129c6101b84040f8f6ae273737c4996afc8199
Tori Kraft <queenvkraft@gmail.com>
2019-10-22 09:45:25 -0500

    chore(redfish): tag
    -Correcting minor bump to micro bump for release/redfish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdcdatamodel',
-    version="1.19.0",
+    version="1.18.1",
     packages=find_packages(),
     install_requires=[
         'pytz==2016.4',
@@ -25,7 +25,7 @@ setup(
         'git+https://github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils',
         'git+https://github.com/NCI-GDC/cdisutils.git@863ce13772116b51bcf5ce7e556f5df3cb9e6f63#egg=cdisutils',
         'git+https://github.com/NCI-GDC/psqlgraph.git@1.3.0#egg=psqlgraph',
-        'git+https://github.com/NCI-GDC/gdcdictionary.git@1.19.0#egg=gdcdictionary',
+        'git+https://github.com/NCI-GDC/gdcdictionary.git@1.18.1#egg=gdcdictionary',
         'git+https://github.com/NCI-GDC/gdc-ng-models.git@1.1.0#egg=gdc_ng_models',
     ],
     entry_points={
