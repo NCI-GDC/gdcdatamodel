a72f80c10b5db29283b6783779121d711450c298
Rowland Ogwara <rogwara@uchicago.edu>
2019-12-02 13:47:37 -0600

    bump version/dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdcdatamodel',
-    version="1.18.1",
+    version="2.0.0-alpha",
     packages=find_packages(),
     install_requires=[
         'pytz',
