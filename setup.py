45a1b0965c832055adf7f454bafcc6e3f75e5b01
Rowland Ogwara <rogwara@uchicago.edu>
2020-04-28 10:26:19 -0500

    feat(study): add new studyrule and released_data models (#309)
    DEV-106: add new studyrule and released_data models
    Added references to studyrule and released_data models from ng_models
    
    Co-authored-by: ProfOak <OpenProfOak@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdcdatamodel',
-    version="2.1.0",
+    version="2.2.0",
     packages=find_packages(),
     install_requires=[
         'pytz',
