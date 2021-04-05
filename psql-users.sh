6bed9b2241cfe9ef3c62f3c5b12ffabde85b4c8c
Rowland Ogwara <rogwara@uchicago.edu>
2021-04-05 14:53:36 -0500

    add dev_models db to jenkins

--- psql-users.sh
+++ psql-users.sh
@@ -1,2 +1,3 @@
 psql -U postgres -c "create user test with superuser password 'test';"
 psql -U postgres -c "create database automated_test with owner test;"
+psql -U postgres -c "create database dev_models with owner test;"
