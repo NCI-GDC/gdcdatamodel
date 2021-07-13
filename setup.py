14bf17b50f025990ca639365f6165ff86b2bee27
Charles Czysz <czysz@uchicago.edu>
2021-07-13 16:10:16 -0500

    Bump requirements

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         "graphviz>=0.4.10,<0.17",
         "jsonschema~=3.2",
         "gdcdictionary @ git+https://github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
-        "gdc-ng-models @ git+https://github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc-ng-models",
-        "psqlgraph @ git+https://github.com/NCI-GDC/psqlgraph.git@3.3.0#egg=psqlgraph",
+        "gdc-ng-models @ git+https://github.com/NCI-GDC/gdc-ng-models.git@1.6.0#egg=gdc-ng-models",
+        "psqlgraph @ git+https://github.com/NCI-GDC/psqlgraph.git@3.4.0#egg=psqlgraph",
     ],
     extras_require={
         'python_version == "2.7"': [
