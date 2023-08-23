11f23f83f70431d8b8109455e98e81278938f60c
Qiao Qiao <68757394+qiaouchicago@users.noreply.github.com>
2023-08-23 12:39:39 -0500

    DEV 2094 drop py36 (#410)
    * drop py36 in piepelines
    
    * remove [dev-]requirements.txt
    
    * update pin for postgres in gitlab-ci

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+from os import path
+
 from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="gdcdatamodel",
-    use_scm_version={
-        "local_scheme": "dirty-tag",
-        "write_to": "gdcdatamodel/_version.py",
-    },
-    setup_requires=["setuptools_scm<6"],
+    description="Repo to keep information about the GDC data model design.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="NCI GDC",
+    author_email="gdc_dev_questions-aaaaae2lhsbell56tlvh3upgoq@cdis.slack.com",
+    url="https://github.com/NCI-GDC/gdcdatamodel",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    license="Apache",
     packages=find_packages(),
     install_requires=[
         "pytz",
