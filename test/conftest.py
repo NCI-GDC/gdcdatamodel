68078733ebf78423f36787270b47a6665a5153e3
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2025-06-23 15:20:45 -0400

    remove pkg_resources so allow python3.12 and 3.13 tox builds

--- test/conftest.py
+++ test/conftest.py
@@ -8,8 +8,8 @@ import logging
 import random
 import unittest
 import uuid
+from importlib import resources
 
-import pkg_resources
 import pytest
 import sqlalchemy
 import yaml
@@ -167,7 +167,9 @@ class BaseTestCase(unittest.TestCase):
 
 @pytest.fixture(scope="module")
 def sample_data():
-    with pkg_resources.resource_stream(__name__, "schema/data/sample.yaml") as f:
+    # with pkg_resources.resource_stream(__name__, "schema/data/sample.yaml") as f:
+    #    graph = yaml.safe_load(f)
+    with resources.files(__name__).joinpath("schema/data/sample.yaml").open("rb") as f:
         graph = yaml.safe_load(f)
 
     f = mocks.GraphFactory(basic, test_models.BasicDictionary)
